b3e940c8f02c46a3994615007bb651fe7e7ca3ea
Dell <swift23e@gmail.com>
2024-04-09 17:17:48 +0300

    added random left picture

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Hello!!!")
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageDice = "dice" + randomNumber1 + ".png";
+
+let randomImageDiceSrc = "images/" + randomImageDice;
+
+let image1 = document.querySelectorAll("img")[0];
+
+// alert(img1);
+image1.setAttribute("src", randomImageDiceSrc);
